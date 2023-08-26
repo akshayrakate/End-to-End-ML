cc31099ca19991681d9481681317a484254759aa
akshay <akshay.rakate001@gmail.com>
2023-08-26 23:23:41 +0530

    delete files

--- src/Components/data_ingestion.py
+++ src/Components/data_ingestion.py
@@ -74,4 +74,4 @@ if __name__ == '__main__':
     # Transforming data
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
-        train_data, test_data)
+        train_data,test_data)
